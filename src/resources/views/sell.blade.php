c689bdc5f197cf7809d40a1513f87fded6590388
kana risaki <kana@rinanoMacBook-Air.local>
2026-05-10 19:57:32 +0900

    sell.blade.phpの修正

--- src/resources/views/sell.blade.php
+++ src/resources/views/sell.blade.php
@@ -3,7 +3,7 @@
 @section('title', '商品出品画面')
 
 @section('css')
-<link rel="stylesheet" href="{{ asset('css/mypage_profile.css') }}" />
+<link rel="stylesheet" href="{{ asset('css/sell.css') }}" />
 @endsection
 
 @section('search')
